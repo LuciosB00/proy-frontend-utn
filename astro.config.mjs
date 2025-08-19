91f81b7e04431d367ba7dfbce506bd3fc09b3389
fhrancoo <132franco@gmail.com>
2025-08-18 22:10:04 -0300

    Initialize Astro project with Svelte and Tailwind

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
+
+import svelte from "@astrojs/svelte";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  integrations: [svelte()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
+});
---
+++ src/components/my-component.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    // Your TypeScript code here
+
+    interface Props {
+        title: string;
+    }
+
+    let { title }: Props = $props();
+</script>
+
+<!-- Your HTML here -->
+
+<h1 class="bg-amber-400 text-red-700 font-bold">{title}</h1>
